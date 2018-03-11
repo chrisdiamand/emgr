b98b6407e5736d7a3c610002630b0fcdc2c2ead6
Chris Diamand <chris@diamand.org>
2018-03-11 20:14:04 +0000

    Make sure EMGR_ENV_CURRENT is exported

--- test/envs/parent1.env.sh
+++ test/envs/parent1.env.sh
@@ -1,6 +1,8 @@
 function t1() {
     fail_if_var_set EMGR_ENV_PARENT
     fail_if_var_not_equal EMGR_ENV_LOCAL parent1
+    fail_if_var_not_exported EMGR_ENV_PARENT
+    fail_if_var_not_exported EMGR_ENV_LOCAL
 }
 
 t1
